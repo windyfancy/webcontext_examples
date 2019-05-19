d3f05d5db7f43cb63bdbe3772f19defd1afdb319
windy <windyfancy9@126.com>
2019-05-20 03:14:21 +0800

    删除多余的文件

--- service/getFiles.js
+++
@@ -1,8 +0,0 @@
-var path=require("path")
-module.exports= {  
-    onRequest() {  
-        var fileName= this.request.query.name;
-        fileName=path.resolve(this.workDirectory,this.config["uploadDir"]+"/"+fileName)
-        this.response.writeFile(fileName);
-    }
-  }
--- service/uploadFiles.js
+++
@@ -1,8 +0,0 @@
-var path=require("path")
-module.exports= {  
-    onRequest() {  
-        var fileName=path.basename(this.request.files.file.path);
-      
-        this.render({fileName:fileName});
-    }
-  }
